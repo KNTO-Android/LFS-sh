a806b731a78dd73ae404c164317f8d33cde9d8c0
KNTO <dntln.gionyuki@gmail.com>
2018-12-02 19:29:25 +0900

    final-script

---
+++ system-build-final-01.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+save_lib="ld-2.28.so libc-2.28.so libpthread-2.28.so libthread_db-1.0.so"
+
+cd /lib
+
+for LIB in $save_lib; do
+    objcopy --only-keep-debug $LIB $LIB.dbg
+    strip --strip-unneeded $LIB
+    objcopy --add-gnu-debuglink=$LIB.dbg $LIB
+done
+
+save_usrlib="libquadmath.so.0.0.0 libstdc++.so.6.0.25
+             libitm.so.1.0.0 libatomic.so.1.2.0" 
+
+cd /usr/lib
+
+for LIB in $save_usrlib; do
+    objcopy --only-keep-debug $LIB $LIB.dbg
+    strip --strip-unneeded $LIB
+    objcopy --add-gnu-debuglink=$LIB.dbg $LIB
+done
+
+unset LIB save_lib save_usrlib
+exec /tools/bin/bash
